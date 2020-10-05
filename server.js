a5f0dd5d8266fc22fba7265142d223616a23dd33
Bhuvana Deepika Karri <bhuvanadeepikakarri@Bhuvanas-MacBook-Pro.local>
2020-10-05 12:24:08 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const { app } = require('./app.js');
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log('server is listening on ', port));
+app.listen(port, () => console.log('server listening on ', port));
